db584153e69caf5127090b01a929dff8c2672f39
Samir Raihan <samirraihan93@gmail.com>
2026-02-26 13:23:32 +0600

    Add casts for is_completed and completed_at in DeploymentChecks model

--- app/Models/DeploymentChecks.php
+++ app/Models/DeploymentChecks.php
@@ -8,6 +8,11 @@ class DeploymentChecks extends Model
 {
     protected $fillable = ['project_id', 'title', 'is_completed', 'completed_at'];
 
+    protected $casts = [
+        'is_completed' => 'boolean',
+        'completed_at' => 'datetime',
+    ];
+
     public function project()
     {
         return $this->belongsTo(Projects::class, 'project_id');
