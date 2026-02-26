cfd0ef0c56b1c84231f3af086f523effd1bd27e2
Samir Raihan <samirraihan93@gmail.com>
2026-02-26 13:23:23 +0600

    Add casts property for release_date in Projects model

--- app/Models/Projects.php
+++ app/Models/Projects.php
@@ -8,6 +8,10 @@ class Projects extends Model
 {
     protected $fillable = ['name', 'owner_email', 'release_date'];
 
+    protected $casts = [
+        'release_date' => 'date',
+    ];
+
     public function deploymentChecks()
     {
         return $this->hasMany(DeploymentChecks::class, 'project_id');
